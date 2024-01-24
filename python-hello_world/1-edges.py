de36552f2448a7502b01231246936e6caf8073b7
SamiyaTol <srastube@gmail.com>
2024-01-24 21:00:37 +0100

    new commit

---
+++ python-hello_world/1-edges.py
@@ -0,0 +1,8 @@
+word = "Holberton"
+
+word_first_3 = word[:3]
+word_last_2 = word[-2:]
+middle_word = word [1:-1]
+print("First 3 letters: "+ word_first_3)
+print("Last 2 letters: "+ word_last_2)
+print("Middle_word: " + middle_word)
