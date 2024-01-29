0f5933f2ec14e651116ce87f80730397ca100605
SamiyaTol <148984827+SamiyaTol@users.noreply.github.com>
2024-01-29 20:59:13 +0100

    Update 2-variable_load.py

--- python-import_modules/2-variable_load.py
+++ python-import_modules/2-variable_load.py
@@ -1,13 +1,4 @@
-#!/usr/bin/python3
-# 5-variable_load.py
-# Brennan D Baraban <375@holbertonschool.com>
-
-if __name__ == "__main__":
-    """Print the value of variable a from variable_load_5."""
-    from variable_load_5 import a
+if _name_ == "_main_":
+    from variable_load_2 import a
 
     print(a)
-    #!/usr/bin/python3
-a = 98
-"""Simple variable
-"""
