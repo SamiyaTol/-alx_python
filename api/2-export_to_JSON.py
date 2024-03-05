07240dfb915d26dd56d8f986b1c56b9c03e8bfe2
SamiyaTol <148984827+SamiyaTol@users.noreply.github.com>
2024-03-05 19:07:59 +0100

    Update 2-export_to_JSON.py

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,41 +1,66 @@
-"""import json, requests, sys"""
+"""
+    This script gathers employee todo data and stores it in JSON format
+"""
+
 import json
 import requests
 import sys
-"""import json, requests, sys"""
 
 
-def getData(id):
+def save_todo_progress_to_json(id):
     """
-    Get data from json api and export to json file
+    fetching employee data
     """
-    usersurl = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    todourl = "{}/todos".format(usersurl)
+    # user details
+    user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
 
-    request1 = requests.get(usersurl)
-    results = request1.json()
-    userid = results['id']
-    username = results['username']
+    # setch user details
+    user_response = requests.get(user_url)
 
-    request2 = requests.get(todourl)
-    tasks = request2.json()
+    # check whether user exists
+    if user_response.status_code != 200:
+        print(f"Employee with ID {id} not found.")
+        return
 
-    alldata = {}
+    # parse response to obtain user data
+    user_data = user_response.json()
+    username = user_data["username"]
 
-    jsondata = [
-            {"task": task['title'], "completed": task['completed'], "username": username}
-            for task in tasks
+    # url for tasks
+    tasks_url = f"https://jsonplaceholder.typicode.com/users/{id}/todos"
+
+    # fetch user's tasks
+    tasks_response = requests.get(tasks_url)
+
+    # check whether the tasks were fetched
+    if tasks_response.status_code != 200:
+        print(f"Unable to fetch TODO list for employee with ID {id}.")
+        return
+
+    # parse to obtain data
+    tasks_data = tasks_response.json()
+
+    # creating json data
+    json_data = {
+        str(id): [
+            {
+                "task": todo["title"],
+                "completed": todo["completed"],
+                "username": username,
+            }
+            for todo in tasks_data
         ]
-    
-    alldata[str(userid)] = jsondata
+    }
 
-    with open("{}.json".format(userid), "w") as jsonfile:
-        json.dump(alldata, jsonfile)
+    # creating a json file
+    json_filename = f"{id}.json"
+    with open(json_filename, "w") as json_file:
+        json.dump(json_data, json_file, indent=4)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        id = sys.argv[1]
-    else:
-        id = 1
-    getData(int(id))
+    # get the user id from command-line argument
+    id = int(sys.argv[1])
+
+    # calling the function to export all data to json file
+    save_todo_progress_to_json(id)
